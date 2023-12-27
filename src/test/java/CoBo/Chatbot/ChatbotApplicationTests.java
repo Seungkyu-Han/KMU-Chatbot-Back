ef70360d1c63a03d17c4c60092ef322763327d55
trust1204@gmail.com <>
2023-12-27 11:07:57 +0900

    first commit

---
+++ src/test/java/CoBo/Chatbot/ChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package CoBo.Chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
