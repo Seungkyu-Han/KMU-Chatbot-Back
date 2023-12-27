4354716250b8e8a2ef5d767a546f7dc040a06c53
trust1204@gmail.com <trust1204@gmail.com>
2023-12-27 17:02:33 +0900

    [feat] add Swagger

---
+++ src/main/java/CoBo/Chatbot/Config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package CoBo.Chatbot.Config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(new Info().title("KMU-Chat bot"))
+                .components(new Components()
+                        .addSecuritySchemes("bear-key",
+                        new io.swagger.v3.oas.models.security.SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+
+
+}
