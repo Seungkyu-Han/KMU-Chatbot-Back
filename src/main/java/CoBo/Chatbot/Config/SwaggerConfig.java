66c3eef5475842a285fd164e79bb6f7411ec3b60
trust1204@gmail.com <trust1204@gmail.com>
2023-12-29 01:05:31 +0900

    [fix] fix Swagger Auth Error

--- src/main/java/CoBo/Chatbot/Config/SwaggerConfig.java
+++ src/main/java/CoBo/Chatbot/Config/SwaggerConfig.java
@@ -3,23 +3,26 @@ package CoBo.Chatbot.Config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearer Auth");
         return new OpenAPI()
                 .info(new Info().title("KMU-Chat bot"))
-                .components(new Components()
-                        .addSecuritySchemes("bear-key",
-                        new io.swagger.v3.oas.models.security.SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")));
+                .components(new Components().addSecuritySchemes("bearer Auth", securityScheme))
+                .security(Collections.singletonList(securityRequirement));
     }
 
 
