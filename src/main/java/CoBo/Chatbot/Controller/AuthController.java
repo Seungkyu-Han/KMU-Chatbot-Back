7b86091e8fbfc7ee1b5bb05f8c30df844eb8e8c2
trust1204@gmail.com <trust1204@gmail.com>
2023-12-28 16:24:31 +0900

    [Comment] add Swagger Description

--- src/main/java/CoBo/Chatbot/Controller/AuthController.java
+++ src/main/java/CoBo/Chatbot/Controller/AuthController.java
@@ -3,6 +3,11 @@ package CoBo.Chatbot.Controller;
 import CoBo.Chatbot.Data.Dto.Auth.Req.AuthPostLoginReq;
 import CoBo.Chatbot.Data.Dto.Auth.Res.AuthPostLoginRes;
 import CoBo.Chatbot.Service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +23,15 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
+    @Operation(summary = "로그인 API", description = "이메일과 비밀번호를 사용하여 토큰을 반환")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공",
+                content = @Content(schema = @Schema(implementation = AuthPostLoginRes.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "없는 계정입니다.",
+                content = @Content()),
+            @ApiResponse(responseCode = "403", description = "비밀번호가 틀렸습니다.",
+                content = @Content())
+    })
     public ResponseEntity<AuthPostLoginRes> postLogin(@RequestBody AuthPostLoginReq authPostLoginReq){
         return authService.postLogin(authPostLoginReq);
     }
--- src/main/java/CoBo/Chatbot/Data/Dto/Auth/Req/AuthPostLoginReq.java
+++ src/main/java/CoBo/Chatbot/Data/Dto/Auth/Req/AuthPostLoginReq.java
@@ -1,5 +1,6 @@
 package CoBo.Chatbot.Data.Dto.Auth.Req;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -7,8 +8,10 @@ import lombok.Data;
 public class AuthPostLoginReq {
 
     @NotBlank
+    @Schema(description = "이메일", example = "jhpmuscle@gmail.com")
     private String email;
 
     @NotBlank
+    @Schema(description = "비밀번호", example = "1234")
     private String password;
 }
